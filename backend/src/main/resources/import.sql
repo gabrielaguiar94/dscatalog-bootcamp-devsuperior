9a1fe00a711a0df449a9c0b28b1bc020a2fd704a
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-09-29 21:26:12 -0300

    Find Category by id, exception handling

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO tb_category(name) VALUES ('Books');
 INSERT INTO tb_category(name) values ('Electronics');
 INSERT INTO tb_category(name) values ('Computers');
+INSERT INTO tb_category(name) values ('Games');
+INSERT INTO tb_category(name) values ('SmartPhones');
+INSERT INTO tb_category(name) values ('Softwares');
