8d457cdfde774ff17714c4128a09d0005efeda18
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-10-10 10:33:24 -0300

    Add buttonIcon to page home

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+import ButtonIcon from '../../core/components/ButtonIcon';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () => (
@@ -7,14 +9,17 @@ const Home = () => (
         <div className="row home-content">
             <div className="col-6">
                 <h1 className="text-title">
-                    Conheça o melhor <br/> catálogo de produtos
+                    Conheça o melhor <br /> catálogo de produtos
                 </h1>
                 <p className="text-subtitle">
-                    Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
+                    Ajudaremos você a encontrar os melhores <br /> produtos disponíveis no mercado.
                 </p>
+                <Link to="/catalog">
+                    <ButtonIcon text="inicie agora a sua busca" />
+                </Link>
             </div>
             <div className="col-6">
-                <MainImage className="main-image"/>
+                <MainImage className="main-image" />
             </div>
         </div>
     </div>
