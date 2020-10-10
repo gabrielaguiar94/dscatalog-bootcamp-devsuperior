d1e2ee60d84bebd21b64998b10a4d947685fe95f
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-10-10 10:46:46 -0300

    Begin page Catalog

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './styles.scss'
 
 const Catalog = () => (
-    <div>
-        <h1>Catalog</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">
+            Catálogo de Produtos
+        </h1>
+        <div className="catalog-products">
+            <h1>produto 1</h1>
+        </div>
     </div>
 
 );
