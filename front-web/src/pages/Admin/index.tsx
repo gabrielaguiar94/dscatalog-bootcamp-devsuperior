8316626f84e9735f920d1ca6ee9cc921333588a8
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-10-15 16:53:47 -0300

    Add routes to admin page

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,11 +1,25 @@
 import NavBar from './components/NavBar';
 import React from 'react';
+import './styles.scss';
+import { Route, Switch } from 'react-router-dom';
 
 const Admin = () => (
-    <div>
+    <div className="admin-container">
         <NavBar />
+        <div className="admin-content">
+            <Switch>
+                <Route path="/admin/products">
+                    <h1>Products</h1>
+                </Route>
+                <Route path="/admin/categories">
+                    <h1>Categories</h1>
+                </Route>
+                <Route path="/admin/users">
+                    <h1>Users</h1>
+                </Route>
+            </Switch>
+        </div>
     </div>
-
 );
 
 export default Admin;
