f15914fcdb78deeeceae01bfcec42506605ae67a
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-11-11 18:59:07 -0300

    Apply login form validations

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './core/components/NavBar';
 import Admin from './pages/Admin';
 import Catalog from './pages/Catalog';
 import ProductDetails from './pages/Catalog/components/ProductDetails';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import history from './core/utils/history';
+
 
 const Routes = () => (
-    <BrowserRouter>
+    <Router  history={history}>
         <NavBar />
         <Switch>
             <Route path="/" exact>
@@ -29,7 +31,7 @@ const Routes = () => (
                 <Admin />
             </Route>
         </Switch>
-    </BrowserRouter>
+    </Router >
 
 );
 
--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,6 +1,7 @@
 import axios, { Method } from 'axios';
 import qs from 'qs';
-import { CLIENT_ID, CLIENT_SECRET } from './auth';
+import { CLIENT_ID, CLIENT_SECRET, getSessionData } from './auth';
+import history from './history';
 
 type RequestParams = {
     method?: Method;
@@ -16,7 +17,16 @@ type LoginData = {
 }
 
 const BASE_URL = 'http://localhost:8080';
-/*const BASE_URL = 'https://gabrielaguiar-dscatalog.herokuapp.com';*/
+
+axios.interceptors.response.use(function (response) {        
+    return response;
+  }, function (error) {
+    if (error.response.status === 401) {
+        history.push('/admin/auth/login')                
+    }
+
+    return Promise.reject(error);
+  });
 
 export const makeRequest = ({ method = 'GET', url, data, params, headers }: RequestParams) => {
     return axios({
@@ -28,6 +38,17 @@ export const makeRequest = ({ method = 'GET', url, data, params, headers }: Requ
     });
 }
 
+export const makePrivateRequest = ({ method = 'GET', url, data, params }: RequestParams) => {
+    const sessionData = getSessionData();
+
+    const headers = {
+        'Authorization': `Bearer ${sessionData.access_token}`
+    }
+
+    return makeRequest({ method, url, data, params, headers });
+
+}
+
 export const makeLogin = (logindata: LoginData) => {
     const token = `${CLIENT_ID}:${CLIENT_SECRET}`;
 
@@ -36,7 +57,7 @@ export const makeLogin = (logindata: LoginData) => {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    const payload = qs.stringify({...logindata,grant_type:'password'})
+    const payload = qs.stringify({ ...logindata, grant_type: 'password' })
 
-    return makeRequest({ url: '/oauth/token', data: payload, method: 'POST', headers});
+    return makeRequest({ url: '/oauth/token', data: payload, method: 'POST', headers });
 }
