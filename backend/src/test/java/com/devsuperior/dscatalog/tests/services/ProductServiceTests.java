6f794c1d99d0f79c77a536bacdf4faebc320ed42
Gabriel Aguiar <Gabriel Aguiar@DESKTOP-RKTB36F>
2021-05-13 13:09:02 -0300

    Fix tests service

--- backend/src/test/java/com/devsuperior/dscatalog/tests/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/services/ProductServiceTests.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscatalog.tests.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,7 +15,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
@@ -50,39 +54,72 @@ public class ProductServiceTests {
 		Mockito.when(repository.find(ArgumentMatchers.any(), ArgumentMatchers.anyString(), ArgumentMatchers.any()))
 				.thenReturn(page);
 		Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+		
 		Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 		Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));
+		
+		Mockito.when(repository.getOne(existingId)).thenReturn(product);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);	
 
 		Mockito.doNothing().when(repository).deleteById(existingId);
-		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+		Mockito.doThrow(EntityNotFoundException.class).when(repository).getOne(nonExistingId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 
 	}
 	
 	@Test
-	public void findByIdShouldThrowWhenIdNotExists() {
+	public void updateShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
 
+		ProductDTO dto = new ProductDTO();
+		
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-			service.findById(nonExistingId);
-		});
-
-		Mockito.verify(repository, Mockito.times(1)).findById(nonExistingId);
+			service.update(nonExistingId, dto);
+		});		
 	}
 
 	@Test
-	public void findByIdShouldReturnProductDtoWhenIdExists() {
+	public void updateShouldReturnProductDTOWhenIdExists() {
+		
+		ProductDTO dto = new ProductDTO();
+		
+		ProductDTO result = service.update(existingId, dto);
+		
+		Assertions.assertNotNull(result);
 
+	}
+	
+	@Test
+	public void findByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.findById(nonExistingId);
+		});		
+	}
+
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExists() {
+		
 		ProductDTO dto = service.findById(existingId);
-
+		
 		Assertions.assertNotNull(dto);
 
 	}
 
 	@Test
-	public void findShouldReturnAllPagedSearch() {
-
+	public void findAllPagedShouldReturnPage() {		
 		
-
+		Long categoryId = 0L;
+		String name = "";
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(categoryId, name, pageRequest);
+		
+		Assertions.assertNotNull(result);		
+		Assertions.assertFalse(result.isEmpty());
+		
+		Mockito.verify(repository, Mockito.times(1)).find(null, name, pageRequest);
+		
+		
 	}
 
 	@Test
