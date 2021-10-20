5f71d995d092b816fd69759762f9588b318b8636
gabrielaguiar94 <gabrielaguiar1994@hotmail.com>
2021-10-20 18:59:01 -0300

    Add ProductCard

---
+++ front-mobile/src/components/ProductCard.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { View, Text, ImageSourcePropType, TouchableOpacity, Image } from 'react-native'
+import { text, theme } from "../styles";
+
+interface ProductProps {
+    id: Number,
+    imgUrl: ImageSourcePropType,
+    name: String,
+    price: Number
+}
+
+const ProductCard: React.FC<ProductProps> = ({ id, imgUrl, name, price }) => {
+    return (
+        <TouchableOpacity style={theme.productCard}>
+            <Image source={imgUrl} />
+            <View style={theme.productDescription}>
+                <Text style={text.productName}>{name}</Text>
+                <View style={theme.priceContainer}>
+                    <Text style={text.currency}>R$</Text>
+                    <Text style={text.productPrice}>{price}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default ProductCard;
--- front-mobile/src/pages/Home.tsx
+++ front-mobile/src/pages/Home.tsx
@@ -1,15 +1,34 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { Image, View, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { text, theme } from '../styles';
 
-const Home: React.FC = ({ navigation }) => {
+import arrow from '../assets/arrow.png';
+import draw from '../assets/draw.png';
+
+const Home: React.FC = () => {
+
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text>Bem vindo</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate('Catalog')}>
-                <Text>Cliquei aqui</Text>
-            </TouchableOpacity>
+        <View style={theme.container}>
+            <View style={theme.card}>
+                <Image source={draw} style={theme.draw} />
+                <View style={theme.textContainer}>
+                    <Text style={text.bold}>Conheça o melhor catálogo de produtos</Text>
+                    <Text style={text.regular}>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</Text>
+                </View>
+                <TouchableOpacity
+                    style={theme.primaryButton}
+                    activeOpacity={0.8}
+                    onPress={() => navigation.navigate('Catalog')}
+
+                >
+                    <Text style={text.primaryText}>INICIE AGORA A SUA BUSCA</Text>
+                    <View style={theme.arrowContainer}>
+                        <Image source={arrow} />
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 };
