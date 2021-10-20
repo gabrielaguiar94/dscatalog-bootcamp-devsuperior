2b1dc47352fb5e3a98e51aa9b0771affb61ca4ed
gabrielaguiar94 <gabrielaguiar1994@hotmail.com>
2021-10-20 15:11:00 -0300

    add home, catalog and routes

---
+++ front-mobile/src/pages/Catalog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Catalog: React.FC = () => {
+    return (
+        <View>
+            <Text>Catalog page</Text>
+        </View>
+    )
+};
+
+export default Catalog;
